9a4872fd5ef6a30d1f1c9a167faad39a81e2ed3c
Ecma Kawarabe🥒 <heppokofrontend@gmail.com>
2021-06-17 01:03:23 +0900

    フィルターUIのJavaScriptとDOM構造を改善

--- docs/js/index.js
+++ docs/js/index.js
@@ -7,7 +7,7 @@ $(function() {
   $('#h_3').hide()
   $('#JPC').hide()
   $('#ENC').hide()
-  $('.button').hide()
+  $('#filter__menu').hide()
   $('#copyright').hide()
   setTimeout(function(){
     $('#h_2').fadeIn(3000);
@@ -25,39 +25,53 @@ setTimeout(function() {
    },1000);
 },9000);
 
-// コメントの出しわけ処理
+// filter
 (function () {
-  $('#menubutton').on('click', function() {
-    $('.button').toggle(100)
-  })
+  /** ドロップダウンを開くためのボタン */
+  const $btn = $('#filter__btn');
+  /** ドロップダウンのコンテナ */
+  const $menu = $('#filter__menu');
+  /** ドロップダウン内のメニュー項目 */
+  const $menuBtns = $('#filter__menu button');
+  const labels = {
+    ja: '日本語のコメント　Japanese Comments　▾',
+    en: '英語のコメント　English Comments　▾',
+    all: 'すべてのコメント　All Comments　▾',
+  };
+
+  $btn.text(labels.all);
+
+  $btn.on('click', function() {
+    $menu.toggle(100);
+  });
+
+  $menuBtns.on('click', function () {
+    const type = this.dataset.type;
+
+    $btn.text(labels[type]);
+    $menu.hide();
+
+    switch (type) {
+      case 'ja':
+        $('.comment_ja').stop(true, true).show(1000);
+        $('.comment_en').stop(true, true).hide(1000);
+
+        break;
+
+      case 'en':
+        $('.comment_en').stop(true, true).show(1000);
+        $('.comment_ja').stop(true, true).hide(1000);
+
+        break;
+
+      default:
+        $('.comment_ja').stop(true, true).show(1000);
+        $('.comment_en').stop(true, true).show(1000);
+
+        break;
+    }
+  });
 })();
-
-$('#button_ja').on('click', function() {
-  $('.button').hide();
-  $('#JPC').show();
-  $('#ENC').hide();
-  $('#AC').hide();
-  $('.comment_ja').stop(true,true).show(1000);
-  $('.comment_en').stop(true,true).hide(1000);
-})
-
-$('#button_en').on('click', function() {
-  $('.button').hide();
-  $('#JPC').hide();
-  $('#ENC').show();
-  $('#AC').hide();
-  $('.comment_en').stop(true,true).show(1000);
-  $('.comment_ja').stop(true,true).hide(1000);
-})
-
-$('#button_all').on('click', function() {
-  $('.button').hide();
-  $('#JPC').hide();
-  $('#ENC').hide();
-  $('#AC').show();
-  $('.comment_ja').stop(true,true).show(1000);
-  $('.comment_en').stop(true,true).show(1000);
-})
 
 $('.skipbutton').on('click', function() {
   $('#h_1').stop(true,true).show()
